e28300c436272e1d3d6e637a887a3b09148902d9
cory-wood-hcp <cory.wood@hashicorp.com>
2026-03-09 16:09:47 -0600

    Update creation.tf

--- creation.tf
+++ creation.tf
@@ -3,4 +3,8 @@ module "account_creation" {
   account_name        = local.account_name
   account_email       = var.account_email
   organizational_unit = var.organizational_unit
+  ou_id_map = {
+    operate = "ou-usuf-qdmvfmf4"
+    sandbox = "ou-usuf-tiqh0htq"
+  }
 }
